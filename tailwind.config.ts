4df6d93c6d9cf2101aa47726283dfedefc4ddc60
Frank <frankmendezresources@gmail.com>
2024-08-12 21:57:11 +0800

    setup development environment

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
+
+export default {
+  content: ['{src,pages,components,app,layout}/**/*!(*.stories|*.spec).{ts,tsx,html}'],
+  theme: {
+    extend: {
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        slidein: 'slidein 1s ease 300ms',
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['light', 'dark'],
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ':root', // The element that receives theme color CSS variables
+  },
+} satisfies Config
