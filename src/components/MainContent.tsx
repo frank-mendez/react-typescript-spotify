061635d54e7f8e32a50a06218d084af84faac290
Frank <frankmendezresources@gmail.com>
2024-08-24 00:05:01 +0800

    feat: web player

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -29,6 +29,12 @@ const MainContent = () => {
               </div>
             </div>
           )}
+          <iframe
+            className="rounded-xl w-full"
+            src="https://open.spotify.com/embed/playlist/37i9dQZEVXbNBz9cRCSFkY?utm_source=generator&theme=0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
         </div>
       )}
     </div>
